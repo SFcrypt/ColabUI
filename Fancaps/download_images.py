29a1f182b699cff448d60149f0401271a6f85d18
Santiago <136390230+SFcrypt@users.noreply.github.com>
2026-01-13 16:00:16 -0600

    Delete Fancaps directory

--- Fancaps/download_images.py
+++
@@ -1,9 +0,0 @@
-# download_images.py
-import os
-import requests
-
-# Función para descargar imagen
-def download_image(url, filename):
-    response = requests.get(url)
-    with open(filename, 'wb') as f:
-        f.write(response.content)
