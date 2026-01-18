0f6a94b36a2a97d594296f8882ba4dca98b3702f
Santiago <136390230+SFcrypt@users.noreply.github.com>
2026-01-18 12:05:48 -0600

    Update animated_status.py

--- Widgets/animated_status.py
+++ Widgets/animated_status.py
@@ -6,6 +6,7 @@ def animated_status(
     background_color="#1E1F21",
     button_colors=("#ed1570", "#f760a8", "#ed1570"),
     title_color="rgba(255,255,255,0.9)",
+    padding="10px 0",
     min_height="120px"
 ):
     """
@@ -32,7 +33,7 @@ def animated_status(
         align-items:center;
         justify-content:center;
         font-family:'Source Sans Pro', sans-serif;
-        padding:10px 0;
+        padding:{padding};
         border-radius:12px;
     ">
         <!-- Título -->
